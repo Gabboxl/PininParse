4ced2128275172d40a630680087edcc557896202
Gabriele <gabboxl0@gmail.com>
2019-11-21 22:38:19 +0100

    added periodi() function + moved the parsing code into parseEDTjs() function

--- src/main/kotlin/ga/gabboxl/pininparse/PininParse.kt
+++ src/main/kotlin/ga/gabboxl/pininparse/PininParse.kt
@@ -1,6 +1,5 @@
 package ga.gabboxl.pininparse
 
-import java.io.File
 import java.net.URL
 
 class PininParse {
@@ -11,30 +10,53 @@ class PininParse {
             val apiResponsePeriodi =
                 URL("https://intranet.itispininfarina.it/orario/_ressource.js").readText().lines()
 
-            for (page in apiResponsePeriodi) {
-                if (page.contains("new Ressource")) {
-                    var pattern = Regex("""listeRessources \[\d+]\s?=\s?new Ressource \(("(.*)",?\s?\n?)+\);""")
-                    var fatt = pattern.find(page)!!.groupValues.get(1)
-                    //println(fatt)
-
-                    var patternSplit = Regex(""",(?=(?:[^"]*"[^"]*")*(?![^"]*"))""")
-                    var values = patternSplit.split(fatt)
-                    //println(fatto2)
-
-                    var currNo = pages.count()
-                    pages.plusAssign(arrayListOf<Any>())
-
-                    var i = 0
-                    while (i < values.count()) {
-
-                        pages[currNo].plusAssign(values[i])
-
-                        i++
-                    }
+            for (line in apiResponsePeriodi) {
+                if (line.contains("new Ressource")) {
+                    val pattern = Regex("""listeRessources \[\d+]\s?=\s?new Ressource \(("(.*)",?\s?\n?)+\);""")
+                    parseEDTjs(pattern, line, pages)
                 }
             }
 
             return pages
         }
+
+        fun periodi(): MutableList<ArrayList<Any>> {
+            var pages = mutableListOf<ArrayList<Any>>()
+
+            val apiResponsePeriodi =
+                URL("https://intranet.itispininfarina.it/orario/_periode.js").readText().lines()
+
+            for (line in apiResponsePeriodi) {
+                if (line.contains("new Periode")) {
+                    val pattern = Regex("""listePeriodes \[\d+]\s?=\s?new Periode \(("(.*)",?\s?\n?)+\);""")
+                    parseEDTjs(pattern, line, pages)
+                }
+            }
+
+            return pages
+        }
+
+        private fun parseEDTjs(
+            pattern: Regex,
+            page: String,
+            pages: MutableList<ArrayList<Any>>
+        ) {
+            var fatt = pattern.find(page)!!.groupValues.get(1)
+
+            var patternSplit = Regex(""",(?=(?:[^"]*"[^"]*")*(?![^"]*"))""")
+            var values = patternSplit.split(fatt)
+
+            var currNo = pages.count()
+            pages.plusAssign(arrayListOf<Any>())
+
+            var i = 0
+            while (i < values.count()) {
+
+                pages[currNo].plusAssign(values[i])
+
+                i++
+            }
+        }
+
     }
 }
