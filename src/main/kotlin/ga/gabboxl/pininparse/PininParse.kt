a7e972bd1a2027bafe11b5b29362dc0b2c0f8327
Gabriele <gabboxl0@gmail.com>
2022-09-08 16:54:39 +0200

    Reformat code

--- src/main/kotlin/ga/gabboxl/pininparse/PininParse.kt
+++ src/main/kotlin/ga/gabboxl/pininparse/PininParse.kt
@@ -7,7 +7,8 @@ class PininParse {
         const val baseLink = "https://orario.itispininfarina.it/"
 
         private fun parseEDTjs(pattern: Regex, line: String, pages: ArrayList<ArrayList<String>>) {
-            val fatt = pattern.find(line)!!.groupValues[2] //forse da sostituire con il gruppo 1 perche mette anche gli apici allinizio e alla fine
+            val fatt =
+                pattern.find(line)!!.groupValues[2] //forse da sostituire con il gruppo 1 perche mette anche gli apici allinizio e alla fine
 
             val patternSplit = Regex("""",(?=([^"]*"[^"]*")*)"""")
             val values = patternSplit.split(fatt)
@@ -26,12 +27,12 @@ class PininParse {
     }
 
 
-     class Update {
-         companion object {
-             var match: String? = null
-         }
+    class Update {
+        companion object {
+            var match: String? = null
+        }
 
-        suspend fun init(){
+        suspend fun init() {
             val apiResponsePeriodi =
                 URL(baseLink + "_bandeau.js").readText().lines()
 
@@ -39,8 +40,9 @@ class PininParse {
 
             for (line in apiResponsePeriodi) {
                 if (line.contains("Aggiornamento")) {
-                        val pattern = Regex("""var dateDerniereMaj = "Aggiornamento: (\d\d\/\d\d\/\d\d\d\d)\";""") //thx to https://regex101.com/
-                        match = pattern.find(line)!!.groupValues[1]
+                    val pattern =
+                        Regex("""var dateDerniereMaj = "Aggiornamento: (\d\d\/\d\d\/\d\d\d\d)\";""") //thx to https://regex101.com/
+                    match = pattern.find(line)!!.groupValues[1]
                 }
             }
         }
@@ -50,61 +52,61 @@ class PininParse {
         }
     }
 
-     class Periodi {
-         companion object {
-             private val pages = arrayListOf<ArrayList<String>>()
-         }
-
-            suspend fun init(){
-                val apiResponsePeriodi =
-                    URL(baseLink + "_periode.js").readText().lines()
-
-                for (line in apiResponsePeriodi) {
-                    if (line.contains("new Periode")) {
-                        if (!line.contains("Selezionate")) { //skippo i periodi falsi
-                            val pattern = Regex("""listePeriodes \[\d+]\s?=\s?new Periode \(("(.*)",?\s?\n?)+\);""")
-                            parseEDTjs(pattern, line, pages)
-                        }
-                    }
-                }
-            }
-
-            fun list(): MutableList<ArrayList<String>> {
-                return pages
-            }
-    }
-
-     class Classi {
+    class Periodi {
         companion object {
             private val pages = arrayListOf<ArrayList<String>>()
         }
 
-            suspend fun init(){
-                val apiResponsePeriodi =
-                    URL(baseLink + "_ressource.js").readText().lines()
+        suspend fun init() {
+            val apiResponsePeriodi =
+                URL(baseLink + "_periode.js").readText().lines()
 
-
-                for (line in apiResponsePeriodi) {
-                    if (line.contains("new Ressource")) {
-                        val pattern = Regex("""listeRessources \[\d+]\s?=\s?new Ressource \(("(.*)",?\s?\n?)+\);""")
+            for (line in apiResponsePeriodi) {
+                if (line.contains("new Periode")) {
+                    if (!line.contains("Selezionate")) { //skippo i periodi falsi
+                        val pattern = Regex("""listePeriodes \[\d+]\s?=\s?new Periode \(("(.*)",?\s?\n?)+\);""")
                         parseEDTjs(pattern, line, pages)
                     }
                 }
-
-                pages.removeAt(0) //levo il primo elemento che contiene selezionate...
-            }
-
-            fun list(): ArrayList<ArrayList<String>> {
-                return pages
-            }
-
-            fun listNomiClassi(): ArrayList<String>{
-                val nomiclassi = arrayListOf<String>()
-
-                for (pagina in pages){
-                    nomiclassi.add(pagina[1])
-                }
-                return nomiclassi
             }
         }
+
+        fun list(): MutableList<ArrayList<String>> {
+            return pages
+        }
+    }
+
+    class Classi {
+        companion object {
+            private val pages = arrayListOf<ArrayList<String>>()
+        }
+
+        suspend fun init() {
+            val apiResponsePeriodi =
+                URL(baseLink + "_ressource.js").readText().lines()
+
+
+            for (line in apiResponsePeriodi) {
+                if (line.contains("new Ressource")) {
+                    val pattern = Regex("""listeRessources \[\d+]\s?=\s?new Ressource \(("(.*)",?\s?\n?)+\);""")
+                    parseEDTjs(pattern, line, pages)
+                }
+            }
+
+            pages.removeAt(0) //levo il primo elemento che contiene selezionate...
+        }
+
+        fun list(): ArrayList<ArrayList<String>> {
+            return pages
+        }
+
+        fun listNomiClassi(): ArrayList<String> {
+            val nomiclassi = arrayListOf<String>()
+
+            for (pagina in pages) {
+                nomiclassi.add(pagina[1])
+            }
+            return nomiclassi
+        }
+    }
 }
