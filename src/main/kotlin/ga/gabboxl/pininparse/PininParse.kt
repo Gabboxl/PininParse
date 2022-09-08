c24c289b5cf96f82cb21d080c1fa9d2c9d8a7038
Gabriele <gabboxl0@gmail.com>
2022-09-08 16:52:44 +0200

    Added Update class to get the latest update date
    + some minor changes

--- src/main/kotlin/ga/gabboxl/pininparse/PininParse.kt
+++ src/main/kotlin/ga/gabboxl/pininparse/PininParse.kt
@@ -4,9 +4,10 @@ import java.net.URL
 
 class PininParse {
     companion object {
+        const val baseLink = "https://orario.itispininfarina.it/"
 
         private fun parseEDTjs(pattern: Regex, line: String, pages: ArrayList<ArrayList<String>>) {
-            val fatt = pattern.find(line)!!.groupValues[2]
+            val fatt = pattern.find(line)!!.groupValues[2] //forse da sostituire con il gruppo 1 perche mette anche gli apici allinizio e alla fine
 
             val patternSplit = Regex("""",(?=([^"]*"[^"]*")*)"""")
             val values = patternSplit.split(fatt)
@@ -22,16 +23,41 @@ class PininParse {
                 i++
             }
         }
-
     }
 
-    class Periodi {
-        companion object {
-            private val pages = arrayListOf<ArrayList<String>>()
+
+     class Update {
+         companion object {
+             var match: String? = null
+         }
+
+        suspend fun init(){
+            val apiResponsePeriodi =
+                URL(baseLink + "_bandeau.js").readText().lines()
+
+
+
+            for (line in apiResponsePeriodi) {
+                if (line.contains("Aggiornamento")) {
+                        val pattern = Regex("""var dateDerniereMaj = "Aggiornamento: (\d\d\/\d\d\/\d\d\d\d)\";""") //thx to https://regex101.com/
+                        match = pattern.find(line)!!.groupValues[1]
+                }
+            }
+        }
+
+        fun list(): String? {
+            return match
+        }
+    }
+
+     class Periodi {
+         companion object {
+             private val pages = arrayListOf<ArrayList<String>>()
+         }
 
             suspend fun init(){
                 val apiResponsePeriodi =
-                    URL("https://orario.itispininfarina.it/_periode.js").readText().lines()
+                    URL(baseLink + "_periode.js").readText().lines()
 
                 for (line in apiResponsePeriodi) {
                     if (line.contains("new Periode")) {
@@ -46,17 +72,16 @@ class PininParse {
             fun list(): MutableList<ArrayList<String>> {
                 return pages
             }
-        }
     }
 
-    class Classi {
-
+     class Classi {
         companion object {
             private val pages = arrayListOf<ArrayList<String>>()
+        }
 
             suspend fun init(){
                 val apiResponsePeriodi =
-                    URL("https://orario.itispininfarina.it/_ressource.js").readText().lines()
+                    URL(baseLink + "_ressource.js").readText().lines()
 
 
                 for (line in apiResponsePeriodi) {
@@ -82,6 +107,4 @@ class PininParse {
                 return nomiclassi
             }
         }
-
-    }
 }
--- src/test/kotlin/test.kt
+++ src/test/kotlin/test.kt
@@ -1,11 +1,14 @@
 import ga.gabboxl.pininparse.PininParse
 
 suspend fun main(){
-    PininParse.Classi.init()
-    PininParse.Periodi.init()
+    //PininParse().Classi().init()
+    //PininParse().Periodi().init()
+    PininParse.Update().init()
 
-    println(PininParse.Classi.list())
-    println(PininParse.Periodi.list())
+    //println(PininParse().Classi().list())
+    //println(PininParse().Periodi().list())
 
-    println(PininParse.Classi.listNomiClassi())
+    //println(PininParse().Classi().listNomiClassi())
+
+    println(PininParse.Update().list())
 }
